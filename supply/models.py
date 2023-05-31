4486e29fc11062c5271cb399171903178acdce78
leehjhjhj <tsukiakarii@naver.com>
2023-05-31 17:20:30 +0900

    fix: 스피너 수정, 사이즈선택 개선 중

---
+++ supply/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from item.models import *
+
+class Order(models.Model):
+    STATUS_CHOICES = (
+        ('waiting', '입금 대기'),
+        ('completed', '입금 완료'),
+        ('shipping', '배송 중'),
+        ('delivered', '배송 완료'),
+    )
+    username = models.CharField(max_length=10)
+    phone = models.IntegerField()
+    email = models.EmailField(max_length=50)
+    grade = models.IntegerField()
+    all_price = models.IntegerField()
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES)
+
+class OrderItems(models.Model):
+    order = models.ForeignKey(Order,related_name='order', on_delete=models.CASCADE)
+    item = models.ForeignKey(Item, related_name='item', on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    size = models.CharField(max_length=10, null=True)
+    
+    
--- supply/views.py
+++ supply/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-
+from item.models import *
 
 def buy(request):
-    return render(request, 'buy.html')
+    all_item = Item.objects.all()
+    context = {
+        'all_item': all_item,
+    }
+    return render(request, 'buy.html', context)
 
