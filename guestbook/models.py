e34b529ea290f26d98e4c34251950911455ea81b
leehjhjhj <tsukiakarii@naver.com>
2023-08-22 14:16:26 +0900

    feat: 방명록 모델 수정

--- guestbook/models.py
+++ guestbook/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 class Balloon(models.Model):
-    writer = models.CharField(max_length=10, null=True)
     body = models.CharField(max_length=50, null=True)
+    created_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"{self.body}"
